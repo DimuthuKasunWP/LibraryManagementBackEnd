4c5ff52bd60c22a2083831ca9d75378c9120cccc
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-11 22:11:21 +0530

    initialized project

---
+++ src/test/java/lk/ac/cmb/ucsc/LibraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.cmb.ucsc.LibraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
