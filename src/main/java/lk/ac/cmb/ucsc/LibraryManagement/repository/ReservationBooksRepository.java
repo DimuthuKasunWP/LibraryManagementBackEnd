456e6863620c9bb6e78145b6fdb0b46fe5966687
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-14 22:06:41 +0530

    reservedbooks repository added

---
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagement/repository/ReservationBooksRepository.java
@@ -0,0 +1,6 @@
+package lk.ac.cmb.ucsc.LibraryManagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservationBooks extends CrudRepository<ReservationBooks,Long> {
+}
