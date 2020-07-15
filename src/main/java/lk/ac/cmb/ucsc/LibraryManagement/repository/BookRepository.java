3f9aa1f8f05faf4b239b79b5f9399a50bd8ad604
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-15 14:33:41 +0530

    add new functions to findbooks

--- src/main/java/lk/ac/cmb/ucsc/LibraryManagement/repository/BookRepository.java
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagement/repository/BookRepository.java
@@ -3,8 +3,14 @@ package lk.ac.cmb.ucsc.LibraryManagement.repository;
 
 import lk.ac.cmb.ucsc.LibraryManagement.entity.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
-public interface BookRepository extends CrudRepository<Book,Integer> {
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book,Integer> , Repository<Book,Integer> {
+
+    public List<Book> findByBookNameAndWriter(String bookName,String writer);
+    public List<Book> findByBookName(String bookName);
 
 
 }
