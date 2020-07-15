864a6cd6703d066f2a8f4a50d28a624506369714
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-15 14:34:24 +0530

    add new function to find reservation by find by Email and books

--- src/main/java/lk/ac/cmb/ucsc/LibraryManagement/repository/ReservationRepository.java
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagement/repository/ReservationRepository.java
@@ -1,7 +1,12 @@
 package lk.ac.cmb.ucsc.LibraryManagement.repository;
 
+import lk.ac.cmb.ucsc.LibraryManagement.entity.Book;
 import lk.ac.cmb.ucsc.LibraryManagement.entity.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends CrudRepository<Reservation,Integer> {
+
+    public List<Reservation> findByEmailAndBook(String email, Book book);
 }
