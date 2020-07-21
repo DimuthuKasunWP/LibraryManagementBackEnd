3f5fafa87448ee9a16ca4acc80fb4a96e2a0fdf5
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-21 12:26:47 +0530

    webconfg and filter added for monitor requests

---
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagement/configuration/WebConfig.java
@@ -0,0 +1,28 @@
+package lk.ac.cmb.ucsc.LibraryManagement.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println("CORS Enabled");
+                registry.addMapping("/api/v1/getAllBooks")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials","Authorization")
+                        .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
+//                        .allowCredentials(true);
+            }
+        };
+    }
+}
