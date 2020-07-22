e194f379d5956eda83b09d0287ef06bc47c95a09
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-22 21:27:32 +0530

    change application properties

--- src/main/java/lk/ac/cmb/ucsc/LibraryManagement/ServletInitializer.java
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagement/ServletInitializer.java
@@ -2,6 +2,12 @@ package lk.ac.cmb.ucsc.LibraryManagement;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +15,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(LibraryManagementApplication.class);
 	}
+
 
 }
--- src/test/java/lk/ac/cmb/ucsc/LibraryManagement/LibraryManagementApplicationTests.java
+++ src/test/java/lk/ac/cmb/ucsc/LibraryManagement/LibraryManagementApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LibraryManagementApplicationTests {
 
-
+	@Test
+	void contextLoads() {
+	}
 
 }
